705d7db4e9903057ebe4e873b5229616776509b4
Yasir1999 <61775885+Yasir1999@users.noreply.github.com>
2021-05-11 22:35:41 +1000

    sign-in, sign-up forms and validation compleated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-//import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./App.css";
+// import styled from "styled-components";
+// import { AccountBox } from "./components/accountBox";
+
+// const AppContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+// function App() {
+//   return (
+//     <AppContainer>
+//       <AccountBox />
+//     </AppContainer>
+//   );
+// }
+
+// export default App;
+
+
+
+import './App.css';
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -9,10 +33,42 @@ import { Charges } from './components/Charges';
 import { AccessRestrictions } from './components/AccessRestrictions';
 import { Help } from './components/Help';
 import { NoMatch } from './components/NoMatch';
-import { Login } from './components/Login';
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+
+
+
+const AppContainer = styled.div`
+  width: 200%;
+  height: 200%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 
 
 class App extends Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    };
+  }
+
+
+
+
+
+
+
+
+
+
   render () {
     return (
       <>
@@ -24,8 +80,9 @@ class App extends Component {
               <Route path="/about" component={About} />
               <Route path="/help" component={Help} />
               <Route path="/accessrestrictions" component={AccessRestrictions} />
-              <Route path="/login" component={Login} />
+              <Route path="/login" component={AccountBox} />
               <Route path="/charges" component={Charges} />
+
               <Route component={NoMatch} />
             </Switch>
           </Router>
@@ -34,5 +91,12 @@ class App extends Component {
       );
 }
 }
+
+
+
+
+
+
+
 
 export default App;
